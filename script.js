bcedef34bf5ff13cd9f7fe621f111875b20f0364
Bamberek <kabaczek99@gmail.com>
2023-04-28 01:15:12 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+var countDownDate = new Date("April 19, 2023 22:18:00").getTime();
+
+var x = setInterval(function () {
+  var now = new Date().getTime();
+
+  var distance = countDownDate - now;
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("demo").innerHTML =
+    days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
+  // If the count down is over, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "0d 0h 0m 0s";
+  }
+}, 1000);
